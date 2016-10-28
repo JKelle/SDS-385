19c1f93b8d38eb85463c17787a28dc86cfe7753f
Josh Kelle <joshua.a.kelle@gmail.com>
2016-10-28 14:51:51 -0500

    did Lasso part

--- exercise5.R
+++ exercise5.R
@@ -21,3 +21,52 @@ mse_func <- function(lambda) {
 
 mse = mapply(mse_func, lambdas)
 plot(lambdas, mse)
+
+# Lasso
+# take away: in-sample mse increases as lambda increases. The fit gets worse.
+
+library(glmnet)
+
+setwd("~/Google Drive/University of Texas/SDS 385; Statistical Models for Big Data/code")
+X = data.matrix(read.csv("../data/diabetesX.csv", header=TRUE), rownames.force=FALSE)
+y = data.matrix(read.csv("../data/diabetesY.csv", header=FALSE), rownames.force=FALSE)
+
+model = glmnet(X, y)
+plot(model, xvar="lambda")
+
+mse = rep(0, length(model$lambda))
+for (i in seq(length(model$lambda), 1)) {
+  beta = as.matrix(model$beta[, i])
+  mse[i] = sum((y - X %*% beta) ^ 2) / length(y)
+}
+plot(model$lambda, mse, log='x')
+
+# cross validation
+# take away: out-of-sample mse has a non-zero optimial value for lambda
+
+n = length(y)
+ntrain = round(0.8 * n)
+trainX = X[1:ntrain, ]
+trainY = y[1:ntrain]
+testX = X[(ntrain + 1):n, ]
+testY = y[(ntrain + 1):n]
+
+model = glmnet(trainX, trainY)
+predictY = predict(model, testX)
+
+mse = rep(0, length(model$lambda))
+for (i in seq(length(model$lambda), 1)) {
+  y_hat = as.matrix(predictY[, i])
+  mse[i] = sum((testY - y_hat) ^ 2) / n
+}
+plot(model$lambda, mse, log='x')
+
+# Cp statistic
+
+mse = rep(0, length(model$lambda))
+for (i in seq(length(model$lambda), 1)) {
+  beta = as.matrix(model$beta[, i])
+  mse = sum((y - X %*% beta) ^ 2) / n
+  
+}
+plot(model$lambda, mse, log='x')
